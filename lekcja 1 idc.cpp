2c3d79d804e344351b50e03185e1a091f181ff42
blumenpl <ittebksz@gmail.com>
2023-10-25 11:53:08 +0200

    Komentarz

---
+++ lekcja 1 idc.cpp
@@ -0,0 +1,11 @@
+ #include <iostream>
+ using namespace std;
+ int main(){
+ 	
+ 	cout <<"Hellow world"<<endl;
+ 	cout <<"Imie";
+ 	
+ 	
+ 	
+	return 0;
+}
---
+++ lekcja 2.cpp
@@ -0,0 +1,22 @@
+//otring zmienna1 = "Wyraz";
+//short liczka = Z;
+//char znak = "A";
+#include <iostream>
+using namespace std;
+
+	int main()
+	{
+		short liczba = 2;
+		float liczbaUzytkownika;
+		cout << "Hello word" << endl;
+		cout << "Podaj liczbe: ";
+		cin >> liczbaUzytkownika;
+		/*
+		kill me pls
+		Nowwww
+		*/
+		cout << endl;
+		cout << "Podana liczba " << liczbaUzytkownika;
+		
+		return 0;
+	}

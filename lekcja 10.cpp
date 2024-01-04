8f94233a0da7d374bbf8bea6e2eedb3175ded26f
blumenpl <ittebksz@gmail.com>
2024-01-04 11:57:25 +0100

    Komentarz

---
+++ lekcja 10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    string imie, nazwisko;
+    cout << "Podaj swoje imie: ";
+    cin >> imie;
+    cout << "Podaj swoje nazwisko: ";
+    cin >> nazwisko;
+    Przywitanie(imie, nazwisko);
+}

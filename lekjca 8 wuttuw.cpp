e9ed9e17e4b0036025b8fad25338a073888cc70d
blumenpl <ittebksz@gmail.com>
2024-01-04 11:26:40 +0100

    Komentarz

---
+++ lekjca 8 wuttuw.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+	int main(){
+	
+int liczba[ 5 ];
+for(int i=0; i <5; i++)
+{
+    cin >> liczba[ i ];
+}
+
+cout << "Podales nastepujace liczby: ";
+for(int i=0; i <5; i++)
+{
+    cout << liczba[ i ] << ", ";
+}
+}

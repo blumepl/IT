68cc10810f916fd3e2aad1373cd4a3044b7a2d0d
blumenpl <ittebksz@gmail.com>
2024-01-08 15:04:03 +0100

    Komentarz

---
+++ lekcja 11.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int liczba;
+    cout << "Podaj liczbe: ";
+    cin >> liczba;
+    switch( liczba )
+    {
+    	case 2:
+    		cout
